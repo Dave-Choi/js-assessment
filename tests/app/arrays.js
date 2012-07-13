d5eec6ad6e266f225c7fe789a352a1aafbe93853
David Choi <daaave@gmail.com>
2012-07-13 00:13:39 -0400

    Completed Array test answers
    Some of the answers feel clunky, but I was trying to speed through
    them.  May refactor later.

--- tests/app/arrays.js
+++ tests/app/arrays.js
@@ -6,19 +6,37 @@ define([
 ], function(answers) {
   describe("arrays", function() {
     var a;
-
     beforeEach(function() {
       a = [ 1, 2, 3, 4 ];
     });
 
+    answers.indexOf = function(array, needle){
+      return array.indexOf(needle);
+    };
     it("you should be able to determine the location of an item in an array", function() {
       expect(answers.indexOf(a, 3)).to.be(2);
     });
 
+    answers.sum = function(array){
+      var i, len = array.length, sum=0;
+      for(i=array.length-1; i>=0; i--){
+        sum += array[i];
+      }
+      return sum;
+    };
     it("you should be able to add the values of an array", function() {
       expect(answers.sum(a)).to.be(10);
     });
 
+    answers.remove = function(array, needle){
+      var i, len = array.length;
+      for(i=array.length-1; i>=0; i--){
+        if(array[i] === needle){
+          array.splice(i, 1);
+        }
+      }
+      return array;
+    };
     it("you should be able to remove a value from an array", function() {
       a.push(2); // Make sure the value appears more than one time
       var result = answers.remove(a, 2);
@@ -27,6 +45,10 @@ define([
       expect(result.join(' ')).to.be('1 3 4');
     });
 
+    answers.append = function(array, item){
+      array.push(item);
+      return array;
+    };
     it("you should be able to add an item to the end of an array", function() {
       var result = answers.append(a, 10);
 
@@ -34,6 +56,10 @@ define([
       expect(result[result.length - 1]).to.be(10);
     });
 
+    answers.truncate = function(array){
+      array.pop();
+      return array;
+    };
     it("you should be able to remove the last item of an array", function() {
       var result = answers.truncate(a);
 
@@ -41,6 +67,9 @@ define([
       expect(result.join(' ')).to.be('1 2 3');
     });
 
+    answers.concat = function(array1, array2){
+      return array1.concat(array2);
+    };
     it("you should be able to join together two arrays", function() {
       var c = [ 'a', 'b', 'c', 1 ],
           result = answers.concat(a, c);
@@ -49,6 +78,10 @@ define([
       expect(result.join(' ')).to.be('1 2 3 4 a b c 1');
     });
 
+    answers.insert = function(array, item, index){
+      array.splice(index, 0, item);
+      return array;
+    };
     it("you should be able to add an item anywhere in an array", function() {
       var result = answers.insert(a, 'z', 2);
 
@@ -56,12 +89,40 @@ define([
       expect(result.join(' ')).to.be('1 2 z 3 4');
     });
 
+    answers.count = function(array, needle){
+      var i, count=0;
+      for(i=array.length-1; i>=0; i--){
+        if(array[i] === needle){
+          count++;
+        }
+      }
+      return count;
+    };
     it("you should be able to count the occurences of an item in an array", function() {
       var result = answers.count([ 1, 1, 1, 2, 1, 3 ], 1);
 
       expect(result).to.be(4);
     });
 
+    answers.duplicates = function(array){
+      var dupes = [];
+      var counts = [];
+      var i;
+      for(i=array.length-1; i>=0; i--){
+        if(counts[array[i]]){
+          counts[array[i]]++;
+        }
+        else{
+          counts[array[i]] = 1;
+        }
+      }
+      for(var k in counts){
+        if(counts[k]>1){
+          dupes.push(k);
+        }
+      }
+      return dupes;
+    };
     it("you should be able to find duplicates in an array", function() {
       var result = answers.duplicates([ 1, 2, 4, 4, 3, 3, 1, 5 ]);
 
@@ -69,6 +130,15 @@ define([
       expect(result.sort().join(' ')).to.be('1 3 4');
     });
 
+    answers.square = function(array){
+      var squares = [];
+      var i, len = array.length;
+      for(i=0; i<len; i++){
+        var val = array[i];
+        squares.push(val * val);
+      }
+      return squares;
+    };
     it("you should be able to square each number in an array", function() {
       var result = answers.square(a);
 
@@ -76,6 +146,16 @@ define([
       expect(result.join(' ')).to.be('1 4 9 16');
     });
 
+    answers.findAllOccurrences = function(array, needle){
+      var occs = [];
+      var i, len = array.length;
+      for(i=0; i<len; i++){
+        if(array[i] === needle){
+          occs.push(i);
+        }
+      }
+      return occs;
+    };
     it("you should be able to find all occurrences of an item in an array", function() {
       var result = answers.findAllOccurrences('abcdefabc'.split(''), 'a');
 
